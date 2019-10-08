97154fee2afda9596aabfcef5701be20006db8ae
robe1606 <robe1606@morris.umn.edu>
2019-10-07 22:19:15 -0500

    Delete main.c and disemvowel_test. Write disemvowel and main functions in file_disemvowel. Mostly complete, but the output still needs to be written into the output file.

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -4,6 +4,8 @@
 #include <ctype.h>
 #include <stdio.h>
 
+#define BUF_SIZE 1024
+
 // Checks if the provided character is a vowel, lower or upper case
 bool isvowel(char const c) {
   char cl = tolower(c);
@@ -15,40 +17,91 @@ bool isvowel(char const c) {
 }
 
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf){
+	int i, len, nvlen, pos;
+	char* outstr;
+	
+	char* instr = in_buf;
+	len = strlen(instr) + 1;
+	// non-vowel length
+	nvlen = 0;
 
-int i, len, nvlen, pos;
-  char* outstr;
-  
-  len = strlen(str) + 1;
+	// Loop through the input string and count the non-vowel characters
+	for(i=0; i<len; ++i) {
+		if(!isvowel(instr[i])) {
+			nvlen++;
+		}
+	}
+	
+	outstr = (char*) calloc(nvlen, sizeof(char));
+	
+	// Tracks position in output string
+	pos = 0;
+	
+	// Loops through input string and adds non-vowel characters to output string
+	for(i=0; i<len; ++i) {
+		if(!isvowel(instr[i])) {
+			outstr[pos] = instr[i];
+			pos++;
+		}
+	}
 
-  // non-vowel length
-  nvlen = 0;
+	out_buf = outstr;
+	return nvlen;
+}
 
-  // Loop through the input string and count the non-vowel characters
-  for(i=0; i<len; ++i) {
-    if(!isvowel(str[i])) {
-      nvlen++;
-    }
-  }
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+	char * buffer = 0;
+	long length;
 
-  outstr = (char*) calloc(nvlen, sizeof(char));
+	fseek (inputFile, 0, SEEK_END);
+	length = ftell (inputFile);
 
-  // Tracks position in output string
-  pos = 0;
+  	fseek (inputFile, 0, SEEK_SET);
+  	buffer = malloc (length);
 
-  // Loops through input string and adds non-vowel characters to output string
-  for(i=0; i<len; ++i) {
-    if(!isvowel(str[i])) {
-      outstr[pos] = str[i];
-      pos++;
-    }
-  }
+  	if (buffer)
+  	{
+    	fread (buffer, 1, length, inputFile);
+  	}
 
-  // return outstr;
-  	return 1;
+  	fclose (inputFile);
+
+  	char * out_buf;
+
+  	int nvlen = copy_non_vowels(length, buffer, out_buf);
+
+  	printf(out_buf);
+
 }
 
 // Takes a string and returns a new string pointer with the vowels removed
 // char *disemvowel(FILE* inputFile, FILE* outputFile) {
 //   return 
 // }
+void main(int argc, char *argv[]) {
+
+	FILE *inputFile;
+    FILE *outputFile;
+
+	if (argc == 1) {
+		inputFile = stdin;
+		outputFile = stdout;
+	}
+
+	if (argc == 2) {
+   		/* Open file for reading*/
+   		inputFile = fopen(argv[1], "r");
+   		outputFile = stdout;
+
+	}
+
+	if (argc == 3) {
+		inputFile = fopen(argv[1], "r");
+
+		/* Open file for writing */
+		outputFile = fopen(argv[2], "w");
+
+	}
+
+	disemvowel(inputFile, outputFile);
+}
