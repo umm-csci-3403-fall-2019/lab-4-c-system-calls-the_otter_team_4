6c37e92e7ab037465b7507fa293bf0c5c9bd49ed
nkfoss <nathankfoss@gmail.com>
2019-10-13 20:57:11 -0500

    Add comment explaining shell commands for summarize_tree

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -1,20 +1,24 @@
 #!/bin/env bash
 
+# Assume no directories or files
 dirCount=0
 regCount=0
 
+# for every directory returned by find, increment the count
 for dir in `find $1 -type d`
 do 
 	dirCount=$((dirCount+1))
 done
 
+# for every regular file returned by find, increment the count
 for reg in `find $1 -type f`
 do
 	regCount=$((regCount+1))
 done
 
-echo $dirCount
-echo $regCount
+
+echo "There were $dirCount directories found."
+echo "There were $regCount regular files found."
 
 
 
